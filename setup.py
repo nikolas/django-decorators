0e85508e27ede6846adf64aac6409f727af7de8d
Nik Nyby <nnyby@columbia.edu>
2015-08-13 15:40:52 -0400

    use https url in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Julian Amaya',
     author_email='julian@monoku.com',
     packages=['django_decorators'],
-    url='http://pypi.python.org/pypi/django-decorators/',
+    url='https://pypi.python.org/pypi/django-decorators/',
     license='LICENSE.md',
     description='A bunch of django extra decorators.',
     long_description=open('README.md').read(),
